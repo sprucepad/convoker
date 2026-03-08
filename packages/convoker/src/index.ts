b1e4b530dbc48fd674f10e6f8dcaf8080cadd66d
sprucepad <sprucepadhq@gmail.com>
2026-03-08 10:28:51 -0300

    docs: add input docs

--- packages/convoker/src/index.ts
+++ packages/convoker/src/index.ts
@@ -1,5 +1,4 @@
 export * from "@convoker/core";
-export * as i from "@convoker/input";
 export * as theme from "@convoker/theme";
 export * as log from "@convoker/log";
 export * as prompt from "@convoker/prompt";
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./command";
 export * from "./error";
+export * as i from "@convoker/input";
